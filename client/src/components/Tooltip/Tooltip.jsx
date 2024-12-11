276e46ee285cb91a48e289aa1ace31468c151fcf
O'Donoghue, Torsten <torstendngh@gmail.com>
2024-12-11 15:18:14 +0100

    implement Tooltip component with customizable tooltip text, position, and styles

--- client/src/components/Tooltip/Tooltip.jsx
+++ client/src/components/Tooltip/Tooltip.jsx
@@ -1,9 +1,43 @@
+import React, { cloneElement, useRef, useId } from "react";
 import styles from "./Tooltip.module.css";
 
-const Tooltip = ({}) => {
-  return (
-    <div className={styles.main}>
+const Tooltip = ({
+  children,
+  tooltip,
+  position = "top",
+  className = "",
+  tooltipClassName = "",
+  id,
+}) => {
+  const triggerRef = useRef(null);
+  const uniqueId = useId();
+  const tooltipId = id || `tooltip-${uniqueId}`;
 
+  if (!tooltip) {
+    return children;
+  }
+
+  const isReactElement = React.isValidElement(children);
+
+  const triggerWithAria = isReactElement
+    ? cloneElement(children, {
+        "aria-describedby": tooltipId,
+        ref: triggerRef,
+      })
+    : children;
+
+  return (
+    <div className={`${styles.tooltipContainer} ${className}`}>
+      {triggerWithAria}
+      <div
+        id={tooltipId}
+        role="tooltip"
+        className={`${styles.tooltip} ${
+          styles[`tooltip--${position}`]
+        } ${tooltipClassName}`}
+      >
+        {tooltip}
+      </div>
     </div>
   );
 };
