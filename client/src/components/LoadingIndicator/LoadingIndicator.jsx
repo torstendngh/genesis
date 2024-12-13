6f0ad08d08ef5630c32b9cf302eb37f5684cfd46
O'Donoghue, Torsten <torstendngh@gmail.com>
2024-12-13 13:02:44 +0100

    implement LoadingIndicator component with spinner animation and accessibility attributes

--- client/src/components/LoadingIndicator/LoadingIndicator.jsx
+++ client/src/components/LoadingIndicator/LoadingIndicator.jsx
@@ -2,8 +2,18 @@ import styles from "./LoadingIndicator.module.css";
 
 const LoadingIndicator = ({}) => {
   return (
-    <div className={styles.main}>
-
+    <div className={styles.main} role="alert" aria-busy="true" aria-live="polite">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <g className={styles.spinner}>
+          <circle cx="12" cy="12" r="9.5" fill="none" strokeWidth="3" />
+        </g>
+      </svg>
     </div>
   );
 };
