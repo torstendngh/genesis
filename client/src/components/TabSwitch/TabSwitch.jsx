ad2d85744802dbcb3f64e91933db3fc31b0b122f
O'Donoghue, Torsten <torstendngh@gmail.com>
2024-12-14 15:36:52 +0100

    implement TabSwitch component with dynamic tab rendering and active state management

--- client/src/components/TabSwitch/TabSwitch.jsx
+++ client/src/components/TabSwitch/TabSwitch.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
 import styles from "./TabSwitch.module.css";
 
-const TabSwitch = ({}) => {
+const sampleData = [
+  {
+    id: 1,
+    title: "Tab 1",
+    titleContent: <>Tab 1</>
+  },
+  {
+    id: 2,
+    title: "Tab 2",
+    titleContent: <>Tab 2</>
+  }
+]
+
+const TabSwitch = ({data, onChange}) => {
+  const [activeTabId, setActiveTabId] = useState(data[0].id);
+  const handleTabClick = (id) => {
+    setActiveTabId(id)
+    onChange(id)
+  }
   return (
     <div className={styles.main}>
-
+      {
+        data.map((tab) => (
+          <button
+            key={tab.id}
+            className={`${styles.tab} ${activeTabId === tab.id ? styles.activeTab : ""}`}
+            onClick={() => handleTabClick(tab.id)}
+            title={tab.title}
+          >
+            {tab.titleContent}
+          </button>
+        ))
+      }
     </div>
   );
 };
