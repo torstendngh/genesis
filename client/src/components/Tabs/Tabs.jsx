aa1d647b7fbf69a2173237d317cd045689ad5a24
Torsten <torstendngh@gmail.com>
2024-12-15 16:10:11 +0100

    refactor Tabs component: migrate TabSwitch functionality and styles, enhancing tab management and responsiveness

--- client/src/components/Tabs/Tabs.jsx
+++ client/src/components/Tabs/Tabs.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
 import styles from "./Tabs.module.css";
 
-const Tabs = ({}) => {
+const sampleData = [
+  {
+    id: 1, // Unique ID for each tab
+    title: "Tab 1", // Title of the tab when hovering
+    titleContent: <>Tab 1</> // Title of the tab like text and icon
+  },
+  {
+    id: 2,
+    title: "Tab 2",
+    titleContent: <>Tab 2</>
+  }
+]
+
+const Tabs = ({data, onChange}) => {
+  const [activeTabId, setActiveTabId] = useState(data[0].id);
+  const handleTabClick = (id) => {
+    setActiveTabId(id)
+    onChange(id)
+  }
   return (
     <div className={styles.main}>
-
+      {
+        data.map((tab) => (
+          <button
+            key={tab.id}
+            className={`${styles.tab} ${activeTabId === tab.id ? styles.activeTab : ""}`}
+            onClick={() => handleTabClick(tab.id)}
+            title={tab.title}
+          >
+            {tab.titleContent}
+          </button>
+        ))
+      }
     </div>
   );
 };
