ac8c6c6a94f5d4338e465f0fc4625095fa218f71
O'Donoghue, Torsten <torstendngh@gmail.com>
2024-12-18 13:23:59 +0100

    refactor Dashboard: remove sample dialog and create Sample component for demonstration

--- client/src/screens/Dashboard/Dashboard.jsx
+++ client/src/screens/Dashboard/Dashboard.jsx
@@ -1,93 +1,11 @@
-import { useState } from "react";
-import Button from "../../components/Button/Button";
-import Dialog from "../../components/Dialog/Dialog";
-import Dropdown from "../../components/Dropdown/Dropdown";
-import Input from "../../components/Input/Input";
-import Link from "../../components/Link/Link";
-import LoadingIndicator from "../../components/LoadingIndicator/LoadingIndicator";
-import Switch from "../../components/Switch/Switch";
-import Tooltip from "../../components/Tooltip/Tooltip";
+import Sample from "../Sample/Sample";
 import styles from "./Dashboard.module.css";
-import Tabs from "../../components/Tabs/Tabs";
-import { AnimatePresence } from "framer-motion";
 
 const Dashboard = ({}) => {
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
   return (
     <div className={styles.main}>
-      {/* ↓ Delete below sample dialog */}
-      <main className={styles.sampleDialog}>
-        <h1>Genesis Client</h1>
-        <h2>Dropdown</h2>
-        <div className={styles.componentContainer}>
-          <Dropdown
-            options={[
-              { label: "Germany", value: "de" },
-              { label: "United States", value: "us" },
-            ]}
-          />
-        </div>
-        <h2>Input</h2>
-        <div className={styles.componentContainer}>
-          <Input label="Test Input" placeholder="This is a placeholder" />
-        </div>
-        <h2>Button</h2>
-        <div className={styles.componentContainer}>
-          <Button>Hello World!</Button>
-        </div>
-        <h2>Tabs</h2>
-        <div className={styles.componentContainer}>
-          <Tabs
-            data={[
-              {
-                id: 1, // Unique ID for each tab
-                title: "Tab 1", // Title of the tab when hovering
-                titleContent: <>Tab 1</>, // Title of the tab like text and icon
-              },
-              {
-                id: 2,
-                title: "Tab 2",
-                titleContent: <>Tab 2</>,
-              },
-              {
-                id: 3,
-                title: "Tab 3",
-                titleContent: <>Tab 3</>,
-              },
-            ]}
-          />
-        </div>
-        <h2>Switch</h2>
-        <div className={styles.componentContainer}>
-          <Switch label={"Switch me on"} />
-        </div>
-        <h2>Link</h2>
-        <div className={styles.componentContainer}>
-          <Link>This is a link</Link>
-        </div>
-        <h2>Loading Indicator</h2>
-        <div className={styles.componentContainer}>
-          <LoadingIndicator />
-        </div>
-        <h2>Tooltip</h2>
-        <div className={styles.componentContainer}>
-          <Tooltip tooltip="This is a tooltip">
-            <Button>Hover over me</Button>
-          </Tooltip>
-        </div>
-        <h2>Dialog</h2>
-        <div className={styles.componentContainer}>
-          <Button onClick={() => setIsDialogOpen(true)}>Open Dialog</Button>
-          <AnimatePresence>
-            {isDialogOpen && (
-              <Dialog onClose={() => setIsDialogOpen(false)}>
-                <p>Dialog content goes here</p>
-              </Dialog>
-            )}
-          </AnimatePresence>
-        </div>
-      </main>
-      {/* ↑ */}
+        {/* Sample shows all the Genesis Components, delete if you want */}
+        <Sample/>
     </div>
   );
 };
